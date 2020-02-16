16ad58c5ac789dd217dae0daa56a7f61dfedfd79
d-goppen <51300326+d-goppen@users.noreply.github.com>
2020-02-17 00:57:54 +0300

    On branch master
     Changes to be committed:
    	new file:   css/main.css
    				Added some custom styles
    	new file:   img/snow.jpg
    				Added stub picture for products without photo
    	modified:   index.html
    				Added container for better UI
    	modified:   js/main.js
    					renderProduct:
    						Parameters is the object now
    						Added parameter for picture address
    						Added default values for parameters
    						Added HTML for a picture
    					renderProducts
    						Commas were printed because renderProducts has returned array.
    						By joining all elements of array in one string commas were deleted.
    						Now renderProducts returns one string instead of array.
    						Removed unsed "const productList"

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
+    { id: 1, title: 'Notebook', price: 20000 },
+    { id: 2, title: 'Mouse', price: 1500 },
+    { id: 3, title: 'Keyboard', price: 5000 },
+    { id: 4, title: 'Gamepad', price: 4500 },
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = ({ title = 'Some product', price = 100000, picture = './img/snow.jpg' }) => {
+    // Parameters is the object now
+    // Added parameter for picture address
+    // Added default values for parameters
+    // Added HTML for a picture
     return `<div class="product-item">
                 <h3>${title}</h3>
+                <img src="${picture}" alt="There are no picturre yet">
                 <p>${price}</p>
                 <button class="by-btn">Добавить в корзину</button>
             </div>`;
 };
 
 const renderProducts = (list) => {
-    const productList = list.map((item) => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productList;
+    // Commas were printed because renderProducts has returned array.
+    // By joining all elements of array in one string commas were deleted.
+    // Now renderProducts returns one string instead of array.
+    // Removed unsed "const productList"
+    document.querySelector('.products').innerHTML = list.map((item) => renderProduct(item)).join('');
 };
 
 renderProducts(products);
